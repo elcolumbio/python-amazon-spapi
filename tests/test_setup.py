08322dfab2793815d45ff554bd9e2c51a7911885
elcolumbio <20401370+elcolumbio@users.noreply.github.com>
2020-11-27 19:32:53 +0100

    include boto3 for now

--- tests/test_setup.py
+++ tests/test_setup.py
@@ -9,3 +9,18 @@ def test_invalid_params():
         setup.Setup(
             endpoint='https://sellingpartnerapi-eu.amazon.com',
             region=None)
+
+
+def test_required_params():
+    """We test the params passing."""
+    # everything which pydantic can cast to a string
+    setup.Setup(
+        role_arn=44.44,  # we have to introduce more rules
+        role_session_name='',
+        client='',
+        secret='',
+        refresh_token='',
+        endpoint='',
+        region='',
+        app_name=''
+    )
